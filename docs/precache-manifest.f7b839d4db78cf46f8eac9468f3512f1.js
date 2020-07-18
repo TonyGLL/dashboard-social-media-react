a02bc669ac28f2dccb5ba02e043dcafed27bf17d
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-07-18 16:30:34 -0500

    docs update

---
+++ docs/precache-manifest.f7b839d4db78cf46f8eac9468f3512f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22a8520f82021ddd511326924213d68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c541648c35c8d51f03a",
+    "url": "/static/css/main.c796bf79.chunk.css"
+  },
+  {
+    "revision": "284a7c0e61a9582c78ad",
+    "url": "/static/js/2.863f1bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.863f1bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c541648c35c8d51f03a",
+    "url": "/static/js/main.fa7bde8d.chunk.js"
+  },
+  {
+    "revision": "fa237b710c7250e5d345",
+    "url": "/static/js/runtime-main.03b5b90a.js"
+  }
+]);
